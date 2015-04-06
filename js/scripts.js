4647762f0e60665e919897edc4e747caca4d4720
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-06 12:30:02 -0700

    add consonant logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 function pigLatin(word) {
   var vowels = ['a', 'e', 'i', 'o', 'u'];
 
+  var consonants = 'bcdfghjklmnpqrstvwxyz';
+
   if ($.inArray(word[0], vowels) !== -1) {
     return word + "ay";
+  } else if ($.inArray(word[0], consonants) !== -1) {
+    do {
+      word = word.substring(1) + word[0];
+    } while ($.inArray(word[0], vowels) === -1);
+    return word + "ay";
   }
+
+
 }
 
 jQuery(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,18 @@
 describe('pigLatin', function() {
-  it("adds ay to end for word that starts with a vowel", function() {
+  it("adds ay to end for word that starts with a vowel a", function() {
     expect(pigLatin("apple")).to.equal("appleay");
+  });
+
+  it("adds ay to end of word that starts with vowel e", function() {
+    expect(pigLatin("earth")).to.equal("earthay");
+  });
+
+  it("moves single consonant to end of word and adds ay", function() {
+    expect(pigLatin("submarine")).to.equal("ubmarinesay");
+  });
+
+  it("moves two consecutive consonants to end of word and adds ay", function() {
+    expect(pigLatin("start")).to.equal("artstay");
   });
 
 });
